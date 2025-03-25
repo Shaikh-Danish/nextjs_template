2758a61fbb573245d0b27f29562048fa80a8fe1b
Shaikh-Danish <shaikhsaalim327@gmail.com>
2025-03-25 10:14:36 +0530

    chore: example env setup

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
+import Home from "@/app/page";
 
-import Home from '@/app/page';
-
-describe('Home Page', () => {
-  it('renders home with role alert', () => {
+describe("Home Page", () => {
+  it("renders home with role alert", () => {
     render(<Home />);
 
-    const home = screen.getByRole('alert');
+    const home = screen.getByRole("alert");
     expect(home).toBeInTheDocument();
   });
 });
